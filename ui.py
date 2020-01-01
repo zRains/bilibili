51c762436d7fe0f5000a598b15fc1968898e685b
zRain <2296342883@qq.com>
2020-01-01 16:51:34 +0800

    Update ui.py

--- ui.py
+++ ui.py
@@ -13,43 +13,52 @@ class meun(QWidget):
     def __init__(self):
         super().__init__()
         self.resize(400, 178)
+        self.setWindowTitle("zRain")
         self.setWindowFlag(QtCore.Qt.WindowMaximizeButtonHint,False)
+        self.setFixedSize(self.width(), self.height())
         self.setui()
         self.action()
     def setui(self):
         self.verticalLayout = QtWidgets.QVBoxLayout(self)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.label = QtWidgets.QLabel("AV号",self)
+        self.label = QtWidgets.QLabel("AVID：",self)
         self.horizontalLayout.addWidget(self.label)
         self.lineEdit = QtWidgets.QLineEdit(self)
         self.horizontalLayout.addWidget(self.lineEdit)
         self.verticalLayout.addLayout(self.horizontalLayout)
         self.qd = QtWidgets.QPushButton("确定",self)
         self.verticalLayout.addWidget(self.qd)
-        self.qx = QtWidgets.QPushButton("取消",self)
+        self.qx = QtWidgets.QPushButton("退出",self)
         self.verticalLayout.addWidget(self.qx)
         self.progressBar = QtWidgets.QProgressBar(self)
-        font = QtGui.QFont()
-        font.setWeight(70)
-        self.progressBar.setFont(font)
         self.progressBar.setAlignment(QtCore.Qt.AlignCenter)
-        self.progressBar.setOrientation(QtCore.Qt.Horizontal)
         self.verticalLayout.addWidget(self.progressBar)
-        self.progressBar.reset()
+        self.horizontalLayout_2 = QtWidgets.QHBoxLayout()
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
+        self.rad1 = QtWidgets.QRadioButton("不进行转换(默认进行转换)",self)
+        self.horizontalLayout_2.addWidget(self.rad1)
+        self.rad2 = QtWidgets.QRadioButton("转换后不删除源文件",self)
+        self.horizontalLayout_2.addWidget(self.rad2) 
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout.addItem(spacerItem1)
     def action(self):
         self.qd.clicked.connect(self.beg)
         self.qx.clicked.connect(lambda : self.close())
     def beg(self):
         if self.lineEdit.text()=="":
             return
-        self.mit=down(self.lineEdit.text())
+        self.mit=down(self.lineEdit.text(),str(self.rad1.isChecked()),str(self.rad2.isChecked()))
         self.mit.data_.connect(self.bar_set)
         self.mit.start()
-    def bar_set(self,value):
+    def bar_set(self,value,stats):
+        all_stats=["下载flv视频文件%p%","下载mp4视频文件%p%","下载m4a音频文件%p%"]
+        self.progressBar.setFormat(all_stats[stats])
         self.progressBar.setValue(value)
         if value==100:
-            time.sleep(3)
             self.progressBar.reset()
+
 
     
 if __name__=='__main__':
